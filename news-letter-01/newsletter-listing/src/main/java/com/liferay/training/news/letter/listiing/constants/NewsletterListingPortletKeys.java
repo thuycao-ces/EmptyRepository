6d743bb58d2cf3da0feeb1ff18ddbc1131ac61e3
thuycao99 <thuy.cao@gs.liferay.com>
2021-05-25 14:50:12 +0700

    Init newsletter listing portlet.

---
+++ news-letter-01/newsletter-listing/src/main/java/com/liferay/training/news/letter/listiing/constants/NewsletterListingPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.news.letter.listiing.constants;
+
+/**
+ * @author ces-user
+ */
+public class NewsletterListingPortletKeys {
+
+	public static final String NEWSLETTERLISTING =
+		"com_liferay_training_news_letter_listiing_NewsletterListingPortlet";
+
+}
---
+++ news-letter-01/newsletter-listing/src/main/java/com/liferay/training/news/letter/listiing/portlet/NewsletterListingPortlet.java
@@ -0,0 +1,30 @@
+package com.liferay.training.news.letter.listiing.portlet;
+
+import com.liferay.training.news.letter.listiing.constants.NewsletterListingPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author ces-user
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=NewsletterListing",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + NewsletterListingPortletKeys.NEWSLETTERLISTING,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class NewsletterListingPortlet extends MVCPortlet {
+}
